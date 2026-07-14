75092e9246a2f2a084b821925f7cb9cb08ddff32
Emir Gasi <emirdeveloper03@gmail.com>
2026-07-14 12:10:38 +0200

    something

--- app/src/main/java/com/emirgasic/forecastfm/feature/playground/ComposePlayground.kt
+++ app/src/main/java/com/emirgasic/forecastfm/feature/playground/ComposePlayground.kt
@@ -1,5 +1,6 @@
 package com.emirgasic.forecastfm.feature.playground
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
@@ -7,17 +8,35 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.emirgasic.forecastfm.R
 
 @Composable
 fun ComposePlayground(modifier:Modifier=Modifier){
-    Column(
-        modifier=modifier.fillMaxSize(),
-        verticalArrangement =Arrangement.Center,
-        horizontalAlignment=Alignment.CenterHorizontally
-    ){
-
-        Text("The app Font")
-
+    Scaffold(){
+        innerPadding->
+        Column(modifier=Modifier.padding(innerPadding)) {
+            Text("Something")
+        }
     }
+
 }
 
